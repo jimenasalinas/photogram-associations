9a61a7469bd015c808b4af40f35a1dfdd4b2339c
Jimena Salinas <111835409+jimenasalinas@users.noreply.github.com>
2023-11-18 02:33:52 +0000

    finishing

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,13 +51,19 @@ class User < ApplicationRecord
   ## Indirect associations
 
   # User#liked_photos: returns rows from the photos table associated to this user through its likes
+  has_many(:liked_photos, through: :likes, source: :photo)
+
 
   # User#commented_photos: returns rows from the photos table associated to this user through its comments
-
+  has_many(:commented_photos, through: :comments, source: :photo)
 
   ### Indirect associations built on scoped associations
 
   # User#followers: returns rows from the users table associated to this user through its accepted_received_follow_requests (the follow requests' senders)
+
+  has_many(:followers, through: :accepted_received_follow_request, :users)
+
+
 
   # User#leaders: returns rows from the users table associated to this user through its accepted_sent_follow_requests (the follow requests' recipients)
 
@@ -89,35 +95,35 @@ class User < ApplicationRecord
   #   return matching_likes
   # end
 
-  def liked_photos
-    my_likes = self.likes
+  # def liked_photos
+  #   my_likes = self.likes
     
-    array_of_photo_ids = Array.new
+  #   array_of_photo_ids = Array.new
 
-    my_likes.each do |a_like|
-      array_of_photo_ids.push(a_like.photo_id)
-    end
+  #   my_likes.each do |a_like|
+  #     array_of_photo_ids.push(a_like.photo_id)
+  #   end
 
-    matching_photos = Photo.where({ :id => array_of_photo_ids })
+  #   matching_photos = Photo.where({ :id => array_of_photo_ids })
 
-    return matching_photos
-  end
+  #   return matching_photos
+  # end
 
-  def commented_photos
-    my_comments = self.comments
+  # def commented_photos
+  #   my_comments = self.comments
     
-    array_of_photo_ids = Array.new
+  #   array_of_photo_ids = Array.new
 
-    my_comments.each do |a_comment|
-      array_of_photo_ids.push(a_comment.photo_id)
-    end
+  #   my_comments.each do |a_comment|
+  #     array_of_photo_ids.push(a_comment.photo_id)
+  #   end
 
-    matching_photos = Photo.where({ :id => array_of_photo_ids })
+  #   matching_photos = Photo.where({ :id => array_of_photo_ids })
 
-    unique_matching_photos = matching_photos.distinct
+  #   unique_matching_photos = matching_photos.distinct
 
-    return unique_matching_photos
-  end
+  #   return unique_matching_photos
+  # end
 
   # def sent_follow_requests
   #   my_id = self.id
